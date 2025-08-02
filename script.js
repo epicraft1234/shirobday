237824245d7fa57cabe0d88d243339f551cd2f5b
Haru <91766298+epicraft1234@users.noreply.github.com>
2025-08-02 22:07:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -84,10 +84,10 @@ function close(){
 
     surat.appendChild(judul);
 
-    content.style.height = "400px";
-    document.getElementById('box').style.height = "400px";
+    content.style.height = "700px";
+    document.getElementById('box').style.height = "700px";
     document.getElementById('com').style.marginTop = "200px";
-    document.getElementById('teks').style.height = "400px";
+    document.getElementById('teks').style.height = "700px";
 
     judul.style.justifyContent = "center";
     judul.style.display = "flex";
@@ -124,6 +124,7 @@ function close(){
 
 
 }
+
 
 
 
